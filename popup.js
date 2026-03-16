b5346c178a8848eff5371830c3b4fc0b360c00df
AlexKorzakov <worksjobs@mail.ru>
2026-03-16 23:57:18 +0300

    init local storage, add alarms

--- popup.js
+++ popup.js
@@ -1,1 +1,18 @@
-console.log("this is a popup!");
+const status = document.querySelector(".status");
+const start = document.querySelector("#start");
+
+chrome.storage.local.get(["enabled"]).then((result) => {
+    const isEnabled = result.enabled ?? false;
+
+    status.textContent = "Status: " + (isEnabled ? "Enabled" : "Disabled");
+});
+
+start.addEventListener("click", (event) => {
+    event.preventDefault();
+    const interval = document.querySelector("#interval").value;
+    console.log(typeof(parseInt(interval)));
+    chrome.storage.local.set({ 
+        enabled: true,
+        interval: parseInt(interval)
+    });
+});
--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,43 @@
-console.log("service-worker");
+chrome.runtime.onInstalled.addListener(async ({ reason }) => {
+    if (reason !== "install") {
+        return;
+    }
+
+    await chrome.storage.local.set({
+        installDate: Date.now(),
+        interval: 20,
+        enabled: false,
+        totalBrakes: 0,
+        history: [],
+    });
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "break-reminder") {
+        chrome.notifications.create(
+            "break-reminder",
+            {
+                type: "basic",
+                iconUrl: "images/sit-tracker-icon-proto.png",
+                title: "SitTrack",
+                message: "Time to wake up",
+            }
+        );
+    }
+});
+
+chrome.notifications.onClicked.addListener((notificationId) => {
+    if (notificationId === "break-reminder") {
+        console.log("User click on notification!");
+    }
+});
+
+
+chrome.storage.onChanged.addListener((chanegs, namespace) => {
+    for (let [key, { oldValue, newValue }] of Object.entries(chanegs)) {
+        console.log(
+        `Storage key "${key}" in namespace "${namespace}" changed.`,
+        `Old value was "${oldValue}", new value is "${newValue}".`
+        );
+    }
+})
