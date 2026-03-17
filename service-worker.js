6ee76ef326dd004b995503287d98e30ee28958c3
AlexKorzakov <worksjobs@mail.ru>
2026-03-17 23:42:13 +0300

    add listeners on storage, alarms, notifications, create messages for service-worker, aphla mvp

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,60 @@
 chrome.runtime.onInstalled.addListener(async ({ reason }) => {
-    if (reason !== "install") {
-        return;
-    }
+    if (reason !== "install") return;
 
     await chrome.storage.local.set({
         installDate: Date.now(),
-        interval: 20,
-        enabled: false,
-        totalBrakes: 0,
-        history: [],
+        intervalMinutes: 20,
+        isRunning: false,
+        totalBreaks: 0,
+        breakHistory: [],
+        notificationClicks: 0,
     });
 });
 
-chrome.alarms.onAlarm.addListener((alarm) => {
-    if (alarm.name === "break-reminder") {
-        chrome.notifications.create(
-            "break-reminder",
-            {
-                type: "basic",
-                iconUrl: "images/sit-tracker-icon-proto.png",
-                title: "SitTrack",
-                message: "Time to wake up",
-            }
-        );
-    }
+chrome.alarms.onAlarm.addListener(async (alarm) => {
+    if (alarm.name !== "break-reminder") return;
+
+    await chrome.notifications.create(
+        "break-reminder",
+        {
+            type: "basic",
+            iconUrl: "images/sit-tracker-icon-proto.png",
+            title: "SitTrack",
+            message: "Time to warm up",
+        }
+    );
+    const { totalBreaks = 0 } = await chrome.storage.local.get("totalBreaks");
+    await chrome.storage.local.set({
+        isRunning: false,
+        endTime: 0,
+        totalBreaks: totalBreaks + 1,
+    });
 });
 
-chrome.notifications.onClicked.addListener((notificationId) => {
+chrome.notifications.onClicked.addListener(async (notificationId) => {
     if (notificationId === "break-reminder") {
-        console.log("User click on notification!");
+        const { notificationClicks = 0 } = await chrome.storage.local.get("notificationClicks");
+        await chrome.storage.local.set({ 
+            notificationClicks: notificationClicks + 1,
+        });
     }
 });
 
-
-chrome.storage.onChanged.addListener((chanegs, namespace) => {
-    for (let [key, { oldValue, newValue }] of Object.entries(chanegs)) {
-        console.log(
-        `Storage key "${key}" in namespace "${namespace}" changed.`,
-        `Old value was "${oldValue}", new value is "${newValue}".`
-        );
+chrome.runtime.onMessage.addListener(async (message) => {
+    if (message.type === "START_TIMER") {
+        await chrome.alarms.clear("break-reminder");
+        await chrome.alarms.create("break-reminder", { delayInMinutes: message.intervalMinutes });
+        const endTime = Date.now() + message.intervalMinutes * 60 * 1000;
+        await chrome.storage.local.set({
+            isRunning: true,
+            intervalMinutes: message.intervalMinutes,
+            endTime: endTime,
+        });
+    } else if (message.type === "STOP_TIMER") {
+        await chrome.alarms.clear("break-reminder");
+        await chrome.storage.local.set({
+            isRunning: false,
+            endTime: 0,
+        });
     }
-})
+});
